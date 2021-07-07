4047516202b0d62bee0589f2124901e438c3d9e4
SeongminLee <seongminlee221@gmail.com>
2021-07-07 22:50:57 +0900

    settings.py edited

--- myblog/settings.py
+++ myblog/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = DEFAULT_APPS + PROJECT_APPS + COMMON_APPS
 
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
